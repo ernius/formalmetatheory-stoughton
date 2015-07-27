83efa00baf145754309c470742041eaf5f42d447
Ernesto Copello <ecopello@gmail.com>
2015-07-27 15:06:42 -0300

    index pages

---
+++ html/auto-index.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# usage: auto-index [dir]
+INDEX=`ls -1 $1 | sed "s/^.*/      <li\>\<a\ href=\"&\"\>&\<\\/a\>\<\\/li\>/"`
+echo "<html>
+  <head>
+<title>Index of all the source code</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">
+    <title>Formalmetatheory-stoughton by ernius</title>
+    <link rel="stylesheet" href="../stylesheets/styles.css">
+    <link rel="stylesheet" href="../stylesheets/github-dark.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script src="../javascripts/respond.js"></script>
+    <!--[if lt IE 9]>
+      <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <!--[if lt IE 8]>
+    <link rel="stylesheet" href="stylesheets/ie.css">
+    <![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+
+</head>
+  <body>
+    <h2>Index of all the source code, one of the main results file is <a href="SubstitutionLemmas.html">SubstitutionLemmas</a> file</h2>
+    <hr>
+    <ui>
+$INDEX
+    <ui>
+  </body>
+</html>"
